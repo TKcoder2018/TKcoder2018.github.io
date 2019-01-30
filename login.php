5d9a3c525800ec47ad37be6bff7ceb602e4dbc4f
TKcoder2018 <47005421+TKcoder2018@users.noreply.github.com>
2019-01-29 21:01:39 -0500

    Update login.php

--- login.php
+++ login.php
@@ -3,7 +3,7 @@
         $pswrd = $_POST['password'];
         
         if ($pswrd == "open") {
-            header('location: 1st_website_main.html');
+            include_once('location: 1st_website_main.html');
             exit;
         } else {
             echo "Sorry wrong password";
