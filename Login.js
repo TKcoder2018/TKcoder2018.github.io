55e4e53a8349326e52e6807de77798f6f8fb7743
TKcoder2018 <47005421+TKcoder2018@users.noreply.github.com>
2019-01-31 13:49:05 -0500

    Update Login.js

--- Login.js
+++ Login.js
@@ -1,7 +1,7 @@
 function password() {
 var testV = 1;
 var pass1 = prompt('Please Enter Your Password',' ');
-while (testV < 3) {
+while (testV < 5) {
 if (!pass1) 
 history.go(-1);
 if (pass1.toLowerCase() == "closed") {
@@ -12,7 +12,7 @@ testV+=1;
 var pass1 = 
 prompt('Access Denied - Password Incorrect, Please Try Again.','Password');
 }
-if (pass1.toLowerCase()!="Enter password" & testV ==3) 
+if (pass1.toLowerCase()!="Enter password" & testV ==5) 
 history.go(-1);
 return " ";
 }
